aef9c879f4f0513bbde1a009055c33e2b644a342
wangzichang <1030506837@qq.com>
2025-03-13 20:32:41 +0800

    Initial Commit

---
+++ AnimageUpscale/AnimageUpscaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimageUpscaleApp.swift
+//  AnimageUpscale
+//
+//  Created by 王梓畅 on 2025/3/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimageUpscaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
